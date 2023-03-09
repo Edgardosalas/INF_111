6aaf193c00b9f4b67f01117612ee054cca6b299d
Edgardosalas <edgarsalasbirrueta@gmail.com>
2023-03-08 23:05:33 -0400

    Practica 7, lotes

---
+++ P7N1.py
@@ -0,0 +1,12 @@
+n=int(input("Introduzca n: "))
+sw=1; sig=1; t=1; p=1
+for i in range(1, n+1):
+    x=int(input("Introduzca x: "))
+    if x==t:
+        print(x)
+        t=t+sig
+        p=p+1
+        if p==3:
+            sig=sig*(-1)
+            p=1
+    
