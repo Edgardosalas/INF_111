d6837e999f5f7b4d2e200917b3c95c254a07ed46
Edgardosalas <edgarsalasbirrueta@gmail.com>
2023-04-11 13:15:52 -0400

    s

--- Java/Laboratorios/Lab1/E1_Rotar_pares_derecha.java
+++ Java/Laboratorios/Lab1/E1_Rotar_pares_derecha.java
@@ -2,31 +2,31 @@ package Laboratorios.Lab1;
 import java.util.Scanner;
 public class E1_Rotar_pares_derecha {
     public static void main(String[] args)
-    {   int n,k,i,x,y,p;
-        k=0; x=0; p=0;
-        int a[]= new int[20];
+    {   int nsbe,ksbe,isbe,xsbe,ysbe,psbe;
+        ksbe=0; xsbe=0; psbe=0;
+        int asbe[]= new int[20];
         try (Scanner sc = new Scanner(System.in)) {
             do
             {   System.out.println("intro tam de vec");
-                n=sc.nextInt();
-            }while (n>20);
-            for(i=1; i<=n; i++)
-            {   System.out.println(" a[" +i+ "]=");
-                a[i]=sc.nextInt();
+                nsbe=sc.nextInt();
+            }while (nsbe>20);
+            for(isbe=1; isbe<=nsbe; isbe++)
+            {   System.out.println(" asbe[" +isbe+ "]=");
+                asbe[isbe]=sc.nextInt();
             }
         }
-        for(i=1;i<=n;i++)
-        {   if (a[i]%2==0)
-            {   if(k==0)
-                {   x=a[i]; p=i; k=1; }
+        for(isbe=1;isbe<=nsbe;isbe++)
+        {   if (asbe[isbe]%2==0)
+            {   if(ksbe==0)
+                {   xsbe=asbe[isbe]; psbe=isbe; ksbe=1; }
                 else
-                {   y=a[i]; a[i]=x; x=y; }
+                {   ysbe=asbe[isbe]; asbe[isbe]=xsbe; xsbe=ysbe; }
             }
         }
-        if(k==1){   a[p]=x; }
+        if(ksbe==1){   asbe[psbe]=xsbe; }
         System.out.println(" ");
-        for (i=1;i<=n;i++)
-        {   System.out.println(" "+ a[i]);
+        for (isbe=1;isbe<=nsbe;isbe++)
+        {   System.out.println(" "+ asbe[isbe]);
         }
     }
 }
